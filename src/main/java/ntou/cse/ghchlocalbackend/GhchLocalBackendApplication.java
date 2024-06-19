506b2e158b14843a53c0547c235b31eddc6bcd51
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-06-19 19:18:13 +0800

    Create Spring Boot project

---
+++ src/main/java/ntou/cse/ghchlocalbackend/GhchLocalBackendApplication.java
@@ -0,0 +1,13 @@
+package ntou.cse.ghchlocalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhchLocalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhchLocalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntou/cse/ghchlocalbackend/GhchLocalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ntou.cse.ghchlocalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhchLocalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
